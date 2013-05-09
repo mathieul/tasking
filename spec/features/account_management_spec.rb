d6b2d82733a76349a9cea4094a0720ebd0f74ab2
Mathieu Lajugie <mathieul@gmail.com>
2013-05-08 23:16:32 -0700

    remove parens

--- spec/features/account_management_spec.rb
+++ spec/features/account_management_spec.rb
@@ -12,13 +12,13 @@ feature "Account management" do
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "123"
     click_button "Sign Up"
-    page.should have_content("Please review the problems below:")
-    page.should have_content("is too short (minimum is 6 characters)")
+    expect(page).to have_content "Please review the problems below:"
+    expect(page).to have_content "is too short (minimum is 6 characters)"
 
     fill_in "Password", with: "verysecret"
     click_button "Sign Up"
-    current_path.should == "/"
-    page.should have_content("Thank you for signing up!")
+    expect(current_path).to eq("/")
+    expect(page).to have_content "Thank you for signing up!"
   end
 
   scenario "Signing in" do
@@ -28,13 +28,13 @@ feature "Account management" do
     fill_in "Email", with: "john@zorn.com"
     fill_in "Password", with: "wrong"
     click_button "Sign In"
-    page.should have_content("Email or password is invalid.")
+    expect(page).to have_content "Email or password is invalid."
 
     fill_in "Password", with: "secret0"
     click_button "Sign In"
-    expect(page).to have_content("Welcome back!")
-    expect(page).to have_css("li > a", text: "Sign Out")
-    expect(page).not_to have_css("li > a", text: "Sign In")
+    expect(page).to have_content "Welcome back!"
+    expect(page).to have_css "li > a", text: "Sign Out"
+    expect(page).not_to have_css "li > a", text: "Sign In"
   end
 
   scenario "Signing out" do
@@ -44,8 +44,8 @@ feature "Account management" do
     click_button "Sign In"
 
     click_link "Sign Out"
-    expect(page).to have_content("Logged out successfully.")
-    expect(page).to have_css("li > a", text: "Sign In")
+    expect(page).to have_content "Logged out successfully."
+    expect(page).to have_css "li > a", text: "Sign In"
   end
 
   scenario "Forgotten password" do
@@ -56,19 +56,19 @@ feature "Account management" do
     click_link "forgotten password?"
     fill_in "Email", with: "john@zorn.com"
     click_button "Reset Password"
-    expect(page).to have_content("Email sent with password reset instructions.")
+    expect(page).to have_content "Email sent with password reset instructions."
 
     open_email "john@zorn.com"
     current_email.click_link "Reset my password"
     fill_in "New Password", with: "verysecret1"
     click_button "Update Password"
-    expect(page).to have_content("Password has been reset!")
+    expect(page).to have_content "Password has been reset!"
     click_link "Sign Out"
 
     click_link "Sign In"
     fill_in "Email", with: "john@zorn.com"
     fill_in "Password", with: "verysecret1"
     click_button "Sign In"
-    expect(page).to have_content("Welcome back!")
+    expect(page).to have_content "Welcome back!"
   end
 end
