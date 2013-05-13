1c4cb515f2f9c356bd4f82c973d32f5f94c40125
Mathieu Lajugie <mathieul@gmail.com>
2013-05-13 06:22:42 -0700

    allow to create a story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,4 +3,22 @@ class StoriesController < ApplicationController
     @new_story = Story.new
     @stories = Story.all
   end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to stories_url, notice: "new story was created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def story_params
+    params
+      .require(:story)
+      .permit(:description, :points, :tech_lead_id, :product_manager_id,
+              :business_driver, :spec_link)
+  end
 end
