08245e5caeefe7b7fb0fc86e495258668a56a0c7
Mathieu Lajugie <mathieu@caring.com>
2013-05-19 19:11:11 -0700

    add has-many associates to the team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,7 @@
 class Team < ActiveRecord::Base
+  has_many :accounts, dependent: :destroy
+  has_many :teammates, dependent: :destroy
+  has_many :stories, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -17,4 +17,24 @@ describe Team do
       expect(team).to have(1).error_on(:name)
     end
   end
+
+  context "associations" do
+    it "has many accounts" do
+      team = create(:team, name: "dev")
+      account = create(:account, team: team)
+      expect(Team.find_by(name: "dev").accounts.to_a).to eq([account])
+    end
+
+    it "has many teammates" do
+      team = create(:team, name: "dev")
+      teammate = create(:teammate, team: team)
+      expect(Team.find_by(name: "dev").teammates.to_a).to eq([teammate])
+    end
+
+    it "has many stories" do
+      team = create(:team, name: "dev")
+      story = create(:story, team: team)
+      expect(Team.find_by(name: "dev").stories.to_a).to eq([story])
+    end
+  end
 end
