9060a93ca98d416ab3eebae5aad92d49d0557230
Mathieu Lajugie <mathieul@gmail.com>
2013-05-27 00:40:41 -0700

    remove scaffold home controller and cleanup routes

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  before_action :authorize, only: :edit
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,17 @@ Tasking::Application.routes.draw do
     end
   end
 
-  # sign-up/in/out links
-  get "sign_up"  => "accounts#new", as: :sign_up
-  get "sign_in"  => "sessions#new", as: :sign_in
-  get "sign_out" => "sessions#destroy", as: :sign_out
-
   # authentication
   resources :accounts, only: [:new, :create] do
     get "activate/:token", as: :activate, on: :collection, action: :activate
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :password_resets, only: [:new, :create, :edit, :update]
+
+  # friendly authentication links
+  get "sign_up"  => "accounts#new", as: :sign_up
+  get "sign_in"  => "sessions#new", as: :sign_in
+  get "sign_out" => "sessions#destroy", as: :sign_out
 
   # backlog
   resources :stories, concerns: :wiselinkable do
@@ -35,47 +35,5 @@ Tasking::Application.routes.draw do
   # config
   resources :teammates, concerns: :wiselinkable
 
-  root "home#index"
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  root to: redirect("/sign_in")
 end
