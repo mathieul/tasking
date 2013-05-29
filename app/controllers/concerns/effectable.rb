475b56337ddb76ff294469ee7d5b12b3cbffc58b
Mathieu Lajugie <mathieul@gmail.com>
2013-05-28 23:47:39 -0700

    update Effectable implementation so effects can be applied to model instances of different kind without conflict

--- app/controllers/concerns/effectable.rb
+++ app/controllers/concerns/effectable.rb
@@ -6,20 +6,24 @@ module Effectable
   end
 
   def classy_effects_for(object)
-    id = object.respond_to?(:id) ? object.id : object
+    summary = summarize_object(object)
     effects.each.with_object([]) do |(effect, target), classes|
-      classes << "trigger-#{effect}" if target == id
+      classes << "trigger-#{effect}" if target == summary
     end.join(" ")
   end
+
+  def trigger_effect!(highlight: nil)
+    effects = (session[:effects] ||= {})
+    effects[:highlight] = summarize_object(highlight) if highlight.present?
+  end
+
+  private
 
   def effects
     @effects ||= session.delete(:effects) || {}
   end
 
-  def trigger_effect!(highlight: nil)
-    effects = (session[:effects] ||= {})
-    if highlight.present?
-      effects[:highlight] = highlight.respond_to?(:id) ? highlight.id : highlight
-    end
+  def summarize_object(object)
+    object.respond_to?(:id) ? "#{object.class.to_s.underscore}-#{object.id}" : object
   end
 end
