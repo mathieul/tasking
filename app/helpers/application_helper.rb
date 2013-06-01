60b379fd72491714dd896b239c2c386480159b12
Mathieu Lajugie <mathieul@gmail.com>
2013-06-01 11:35:58 -0700

    allow to set page title and page description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(title = nil)
+    @title = title unless title.nil?
+    @title || "Tasking"
+  end
+
+  def page_description(content = nil, &block)
+    content_for(:page_description, content, &block)
+  end
 end
