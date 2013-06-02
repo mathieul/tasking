ec8688e4307e8e7214506f9665db55ffb7c22511
Mathieu Lajugie <mathieul@gmail.com>
2013-06-01 21:08:49 -0700

    try out a bunch of columns on the sprint edit page

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -29,6 +29,7 @@ class SprintsController < ApplicationController
       redirect_to stories_path, error: "There is no #{sprint_label}."
     end
     @sprint = sprint.decorate
+    @columns = 15
   end
 
   private
