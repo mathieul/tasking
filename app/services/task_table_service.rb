56852cd82606b01afc051b11a6a3c4c12d98e02c
Mathieu Lajugie <mathieul@gmail.com>
2013-06-05 06:49:19 -0700

    render sprint task table

--- app/services/task_table_service.rb
+++ app/services/task_table_service.rb
@@ -1,16 +1,16 @@
 class TaskTableService
   attr_reader :taskable_stories
-  attr_reader :num_columns, :column_width
+  attr_reader :cols, :col_width
 
   def initialize(taskable_stories)
     @taskable_stories = taskable_stories
-    @num_columns = 2
-    @column_width = 145
+    @cols = 3
+    @col_width = 145
   end
 
   def render_header(kind)
     %Q{
-      <th colspan="#{num_columns}" class="border-left" style="width: #{column_width * num_columns}px">
+      <th colspan="#{cols}" class="border-left">
       #{kind.to_s.humanize}
       </th>
       }.html_safe
@@ -19,16 +19,16 @@ class TaskTableService
   def each_story_and_tasks(&block)
     taskable_stories.each do |taskable_story|
       tasks = {
-        todo: [nil] * num_columns,
-        in_progress: [nil] * num_columns,
-        done: [nil] * num_columns
+        todo: [nil] * cols,
+        in_progress: [nil] * cols,
+        done: [nil] * cols
       }
       block.call(taskable_story.story.decorate, tasks)
     end
   end
 
   def table_properties(existing_width)
-    width = existing_width + column_width * 3 * num_columns
+    width = existing_width + col_width * 3 * cols
     {style: "width: #{width}px;"}
   end
 end
