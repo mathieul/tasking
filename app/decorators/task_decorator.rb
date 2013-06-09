0e1a4686bba3834c2a5551a9083a33eea714925b
Mathieu Lajugie <mathieul@gmail.com>
2013-06-09 01:10:20 -0700

    when editing a sprint show decimals only when necessary

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -1,7 +1,15 @@
 class TaskDecorator < ApplicationDecorator
   delegate_all
 
+  def duration
+    object.hours == object.hours.to_i ? object.hours.to_i : object.hours
+  end
+
   def timed_description
-    [object.description, object.hours.zero? ? nil : "#{object.hours}h"].compact.join(" ")
+    if object.hours.zero?
+      object.description
+    else
+      "#{object.description} #{duration}h"
+    end
   end
 end
--- spec/decorators/task_decorator_spec.rb
+++ spec/decorators/task_decorator_spec.rb
@@ -4,10 +4,17 @@ describe TaskDecorator do
   let(:task)      { double }
   let(:decorator) { TaskDecorator.decorate(task) }
 
+  it "returns the duration as an int if no decimal with #duration" do
+    task.stub(hours: 12.0)
+    expect(decorator.duration).to eq(12)
+    task.stub(hours: 12.5)
+    expect(decorator.duration).to eq(12.5)
+  end
+
   it "returns description and duration with #timed_description" do
     task.stub(description: "the thing to do", hours: 0)
     expect(decorator.timed_description).to eq("the thing to do")
-    task.stub(hours: 12)
+    task.stub(hours: 12.0)
     expect(decorator.timed_description).to eq("the thing to do 12h")
   end
 end
