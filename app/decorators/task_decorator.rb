6f722daf1591b9e69beb127c5e3f9fc29d34442f
Mathieu Lajugie <mathieul@gmail.com>
2013-06-08 17:14:37 -0700

    add task decorator spec

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -2,7 +2,6 @@ class TaskDecorator < ApplicationDecorator
   delegate_all
 
   def timed_description
-    binding.pry if object.description == "do something 1h"
-    [object.description, object.hours.zero? ? nil : "#{object.hours}h"].join(" ")
+    [object.description, object.hours.zero? ? nil : "#{object.hours}h"].compact.join(" ")
   end
 end
--- spec/decorators/task_decorator_spec.rb
+++ spec/decorators/task_decorator_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe TaskDecorator do
+  let(:task)      { double }
+  let(:decorator) { TaskDecorator.decorate(task) }
+
+  it "returns description and duration with #timed_description" do
+    task.stub(description: "the thing to do", hours: 0)
+    expect(decorator.timed_description).to eq("the thing to do")
+    task.stub(hours: 12)
+    expect(decorator.timed_description).to eq("the thing to do 12h")
+  end
 end
