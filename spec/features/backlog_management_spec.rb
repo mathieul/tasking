37ebe9f2dbd9a309b20d3de248618f6f0c2cd6a0
Mathieu Lajugie <mathieul@gmail.com>
2013-06-09 14:09:41 -0700

    fix backlog create story feature

--- spec/features/backlog_management_spec.rb
+++ spec/features/backlog_management_spec.rb
@@ -18,18 +18,19 @@ feature "Backlog management" do
     # expect(page).to have_content("New user story")
 
     fill_in "Description", with: "As a user I can do something so I benefit from it"
+    click_on "8"
     select "Ari", from: "Tech lead"
     select "Sophie", from: "Product manager"
     fill_in "Business driver", with: "usability"
     fill_in "Spec link", with: "http://www.google.com"
     click_on "Create story"
-    within "tbody.table tr:nth(1)" do
-      expect(page).to have_css("> td:nth(2)", text: "Ari")
-      expect(page).to have_css("> td:nth(3)", text: "Sophie")
-      expect(page).to have_css("> td:nth(4)", text: "usability")
-      expect(page).to have_css("> td:nth(5)", text: "As a user I can do something so I benefit from it")
-      expect(page).to have_css("> td:nth(6)", text: "3")
-      expect(page).to have_css("> td:nth(7)", text: "http://www.google.com")
+    within "tbody.table tr:nth-child(2)" do
+      expect(page).to have_css("td:nth-child(2)", text: "Ari")
+      expect(page).to have_css("td:nth-child(3)", text: "Sophie")
+      expect(page).to have_css("td:nth-child(4)", text: "usability")
+      expect(page).to have_css("td:nth-child(5)", text: "As a user I can do something so I benefit from it")
+      expect(page).to have_css("td:nth-child(6)", text: "8")
+      expect(page).to have_css("td:nth-child(7)", text: "http://www.google.com")
     end
   end
 end
