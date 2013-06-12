4928db26ec2d5daffd4920df3135b3085c3cf194
Mathieu Lajugie <mathieu@caring.com>
2013-06-11 17:39:53 -0700

    remove puma config not taken into account

--- config/puma.rb
+++
@@ -1,9 +0,0 @@
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-threads 4,4
-
-bind  "unix:///home/deploy/apps/tasking/shared/tmp/puma/tasking-puma.sock"
-pidfile "/home/deploy/apps/tasking/current/tmp/puma/pid"
-state_path "/home/deploy/apps/tasking/current/tmp/puma/state"
-
-activate_control_app
