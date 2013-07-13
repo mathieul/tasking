6187f09da0f232ddbfdf320adc11638dedb8f1d4
Mathieu Lajugie <mathieul@gmail.com>
2013-07-13 13:55:39 -0700

    allow to create stories using new remote form flow

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,18 +6,25 @@ class StoriesController < ApplicationController
   before_action :find_story, only: [:update, :update_position, :destroy]
 
   def index
-    @new_story = Story.new
-    @stories = @team.stories.ranked.backlogged.decorate
-    @velocity = VelocityService.new(@team.projected_velocity, @stories)
+    setup_to_render_main
+  end
+
+  def new
+    @story = Story.new
+    setup_to_render_main
   end
 
   def create
     @story = @team.stories.build(story_params)
-    if @story.save
-      trigger_effect!(highlight: @story)
-      redirect_to stories_url
-    else
-      render_index_action
+    respond_to do |format|
+      if @story.save
+        notice = "New story was created."
+        format.html { redirect_to stories_url, notice: notice }
+        format.js   { render_refresh_main(notice: notice, auto_close: true, updated: @story) }
+      else
+        format.html { setup_to_render_main; render :new }
+        format.js
+      end
     end
   end
 
@@ -26,7 +33,8 @@ class StoriesController < ApplicationController
       trigger_effect!(highlight: @story)
       redirect_to stories_url
     else
-      render_index_action
+      setup_to_render_main
+      render :index
     end
   end
 
@@ -38,7 +46,11 @@ class StoriesController < ApplicationController
 
   def destroy
     @story.destroy
-    redirect_to stories_url, notice: "Story #{@story.description.inspect} was deleted."
+    warning = "Story ##{@story.id} was deleted."
+    respond_to do |format|
+      format.html { redirect_to stories_url, warning: warning }
+      format.js   { render_refresh_main(warning: warning) }
+    end
   end
 
   def update_velocity
@@ -50,9 +62,30 @@ class StoriesController < ApplicationController
 
   private
 
-  def render_index_action
-      index
-      render :index
+  def setup_to_render_main
+    @stories = @team.stories.ranked.backlogged.decorate
+    @velocity = VelocityService.new(@team.projected_velocity, @stories)
+  end
+
+  def render_refresh_main(options = {})
+    url = options.delete(:url) || stories_url
+    setup_to_render_main
+    render template: "shared/refresh_main", locals: {
+      content: {partial: "stories/stories", stories: @stories, velocity: @velocity},
+      updated_id: options[:updated] && dom_id(options[:updated]),
+      flash: options_to_flash(options),
+      redirect_url: url
+    }
+  end
+
+  def options_to_flash(options)
+    supported_types = %i[info notice warning error]
+    if (type = options.keys.first { |type| type.in?(supported_types) })
+      message = options.delete(type)
+      options.merge(type: type == :notice ? :success : type, message: message)
+    else
+      {}
+    end
   end
 
   def story_params
