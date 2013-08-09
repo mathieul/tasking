0a3a0014bd5c2fb6c72b8a338152aee985a65037
Mathieu Lajugie <mathieu@caring.com>
2013-08-08 18:31:22 -0700

    WIP: update pubsub server to support sessions

--- wspubsub/lib/wspubsub/pubsub.ex
+++ wspubsub/lib/wspubsub/pubsub.ex
@@ -1,33 +1,53 @@
 defmodule Wspubsub.Pubsub do
 
   defmodule State do
-    def blank, do: { _registered = [], _last_messages = []}
+    def blank, do: { _sessions = HashDict.new, _last_messages = []}
 
-    def add_registered(state, pid) do
-      { registered, last_messages } = state
-      { [ pid | registered ], last_messages }
+    def add_registered(state, pid, sid) do
+      { sessions, last_messages } = state
+      session = fetch_session(sessions, sid)
+      { update_session(sessions, sid, [ pid | session ]), last_messages }
     end
 
-    def del_registered(state, pid) do
-      { registered, last_messages } = state
-      { Enum.reject(registered, &1 == pid), last_messages }
+    def del_registered(state, pid, sid) do
+      { sessions, last_messages } = state
+      session = fetch_session(sessions, sid)
+      session = Enum.reject(session, &(&1 == pid))
+      { session, last_messages }
     end
 
-    def last_messages(state) do
-      { _r, last_messages } = state
-      last_messages
+    # def last_messages(state) do
+    #   { _r, last_messages } = state
+    #   last_messages
+    # end
+
+    def session(state, sid) do
+      fetch_session(sessions(state), sid)
     end
 
-    def registered(state) do
-      { registered, _l } = state
-      registered
+    def sessions(state) do
+      { sessions, _l } = state
+      sessions
     end
 
-    def add_message(state, message) do
-      { registered, last_messages } = state
-      remaining = Enum.take(last_messages, 9)
-      { registered, [message | remaining] }
+    def session_ids(state) do
+      Dict.keys(sessions(state))
+    end    
+
+    # def add_message(state, message) do
+    #   { registered, last_messages } = state
+    #   remaining = Enum.take(last_messages, 9)
+    #   { registered, [message | remaining] }
+    # end
+
+    defp fetch_session(sessions, sid) do
+      case Dict.fetch(sessions, sid) do
+        { :ok, session } -> session
+        :error -> []
+      end
     end
+
+    defp update_session(sessions, sid, session), do: Dict.put(sessions, sid, session)
   end
 
   use GenServer.Behaviour
@@ -39,11 +59,14 @@ defmodule Wspubsub.Pubsub do
     :gen_server.start_link({ :local, :pubsub }, __MODULE__, nil, [])
   end
 
-  def register(pid, send_last // false), do: :gen_server.call(:pubsub, { :register, pid, send_last })
-  def unregister(pid), do: :gen_server.call(:pubsub, { :unregister, pid })
-  def publish(message), do: :gen_server.cast(:pubsub, { :publish, message })
-  def last_messages, do: :gen_server.call(:pubsub, :last_messages)
-  def register_list, do: :gen_server.call(:pubsub, :register_list)
+  def register(pid, sid, send_last // false) do
+    :gen_server.call(:pubsub, { :register, pid, sid, send_last })
+  end
+  def unregister(pid, sid), do: :gen_server.call(:pubsub, { :unregister, pid, sid })
+  def session_ids, do: :gen_server.call(:pubsub, :session_ids)
+  # def publish(message), do: :gen_server.cast(:pubsub, { :publish, message })
+  # def last_messages, do: :gen_server.call(:pubsub, :last_messages)
+  def register_list(sid), do: :gen_server.call(:pubsub, { :register_list, sid })
 
   #####
   # GenServer implementation
@@ -52,33 +75,37 @@ defmodule Wspubsub.Pubsub do
     { :ok, State.blank }
   end
 
-  def handle_call({ :register, pid, send_last }, _from, state) do
+  def handle_call({ :register, pid, sid, send_last }, _from, state) do
     if send_last do
       messages = Enum.reverse(State.last_messages(state))
       Enum.each messages, fn message ->
         pid <- { :pubsub_message, message }
       end
     end
-    { :reply, :registered, State.add_registered(state, pid) }
+    { :reply, :registered, State.add_registered(state, pid, sid) }
   end
 
-  def handle_call({ :unregister, pid }, _from, state) do
-    { :reply, :unregistered, State.del_registered(state, pid) }
+  def handle_call({ :unregister, pid, sid }, _from, state) do
+    { :reply, :unregistered, State.del_registered(state, pid, sid) }
   end
 
-  def handle_call(:last_messages, _from, state) do
-    { :reply, State.last_messages(state), state }
+  def handle_call(:session_ids, _from, state) do
+    { :reply, State.session_ids(state) , state }
   end
 
-  def handle_call(:register_list, _from, state) do
-    list = Enum.map(State.registered(state), inspect(&1))
+  # def handle_call(:last_messages, _from, state) do
+  #   { :reply, State.last_messages(state), state }
+  # end
+
+  def handle_call({ :register_list, sid }, _from, state) do
+    list = Enum.map(State.session(state, sid), inspect(&1))
     { :reply, list, state }
   end
 
-  def handle_cast({ :publish, message }, state) do
-    Enum.each State.registered(state), fn pid ->
-      pid <- { :pubsub_message, message }
-    end
-    { :noreply, State.add_message(state, message) }
-  end
+  # def handle_cast({ :publish, message }, state) do
+  #   Enum.each State.registered(state), fn pid ->
+  #     pid <- { :pubsub_message, message }
+  #   end
+  #   { :noreply, State.add_message(state, message) }
+  # end
 end
--- wspubsub/test/pubsub_test.exs
+++ wspubsub/test/pubsub_test.exs
@@ -4,38 +4,41 @@ defmodule PubsubTest do
   use ExUnit.Case
   alias Wspubsub.Pubsub
 
-  teardown do
-    Pubsub.unregister(self)
-    :ok
+  test "it returns the list of session ids with #session_ids" do
+    Pubsub.register(self, "abc123")
+    assert Pubsub.session_ids == ["abc123"]
+    Pubsub.unregister(self, "abc123")
   end
 
-  test "it can return the list of registered pids" do
-    Pubsub.register(self)
-    assert Pubsub.register_list == [inspect self]
+  test "it can return the list of registered pids of a session" do
+    Pubsub.register(self, "ou812")
+    assert Pubsub.register_list("ou812") == [inspect self]
+    Pubsub.unregister(self, "ou812")
   end
 
   test "it receives message published when registered" do
-    Pubsub.register(self)
-    Pubsub.publish("Hello there" )
+    Pubsub.register(self, "ou812")
+    Pubsub.publish("Hello there", "ou812")
     receive do
       { :pubsub_message, message } -> assert message == "Hello there"
     after 50 -> raise "Did not receive message published"
     end
+    Pubsub.unregister(self, "ou812")
   end
 
-  test "it doesn't receive messages after it has unregistered" do
-    Pubsub.register(self)
-    Pubsub.unregister(self)
-    Pubsub.publish("Hello there")
-    receive do
-      message -> raise "Received unexpected message #{inspect message} while unregistered"
-    after 100 -> assert "Nothing received"
-    end
-  end
+  # test "it doesn't receive messages after it has unregistered" do
+  #   Pubsub.register(self)
+  #   Pubsub.unregister(self)
+  #   Pubsub.publish("Hello there")
+  #   receive do
+  #     message -> raise "Received unexpected message #{inspect message} while unregistered"
+  #   after 100 -> assert "Nothing received"
+  #   end
+  # end
 
-  test "the server remembers the last 10 messages" do
-    Enum.each 1..12, fn n -> Pubsub.publish("msg#{n}") end
-    expected = Enum.map 12..3, fn n -> "msg#{n}" end
-    assert Pubsub.last_messages == expected
-  end
+  # test "the server remembers the last 10 messages" do
+  #   Enum.each 1..12, fn n -> Pubsub.publish("msg#{n}") end
+  #   expected = Enum.map 12..3, fn n -> "msg#{n}" end
+  #   assert Pubsub.last_messages == expected
+  # end
 end
